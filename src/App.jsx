e1dd78a0b398cb2b836b5ef561bb80a49354df9c
Nuren Shams Chowdhury <72270901+whyshams@users.noreply.github.com>
2023-11-05 04:47:40 +0600

    Updating image URL

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import Gallery from "./Components/Gallery";
 function App() {
   //the below array is consisted with the photos provided by Oyollo
   const photos = [
-    "/src/assets/images/image-1.webp",
-    "/src/assets/images/image-2.webp",
-    "/src/assets/images/image-3.webp",
-    "/src/assets/images/image-4.webp",
-    "/src/assets/images/image-5.webp",
-    "/src/assets/images/image-6.webp",
-    "/src/assets/images/image-7.webp",
-    "/src/assets/images/image-8.webp",
-    "/src/assets/images/image-9.webp",
-    "/src/assets/images/image-10.jpeg",
-    "/src/assets/images/image-11.jpeg",
+    "https://raw.githubusercontent.com/whyshams/ollyo-gallery-task/main/src/assets/images/image-1.webp",
+    "https://raw.githubusercontent.com/whyshams/ollyo-gallery-task/main/src/assets/images/image-2.webp",
+    "https://raw.githubusercontent.com/whyshams/ollyo-gallery-task/main/src/assets/images/image-3.webp",
+    "https://raw.githubusercontent.com/whyshams/ollyo-gallery-task/main/src/assets/images/image-4.webp",
+    "https://raw.githubusercontent.com/whyshams/ollyo-gallery-task/main/src/assets/images/image-5.webp",
+    "https://raw.githubusercontent.com/whyshams/ollyo-gallery-task/main/src/assets/images/image-6.webp",
+    "https://raw.githubusercontent.com/whyshams/ollyo-gallery-task/main/src/assets/images/image-7.webp",
+    "https://raw.githubusercontent.com/whyshams/ollyo-gallery-task/main/src/assets/images/image-8.webp",
+    "https://raw.githubusercontent.com/whyshams/ollyo-gallery-task/main/src/assets/images/image-9.webp",
+    "https://raw.githubusercontent.com/whyshams/ollyo-gallery-task/main/src/assets/images/image-10.jpeg",
+    "https://raw.githubusercontent.com/whyshams/ollyo-gallery-task/main/src/assets/images/image-11.jpeg",
   ];
   return (
     <>
